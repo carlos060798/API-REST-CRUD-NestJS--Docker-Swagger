f5c7e166bae274f389acaaa06241d8dff40312b9
carlos060798 <daniloangaritagarcia@gmail.com>
2024-07-21 14:37:09 -0500

    prueba 1

--- src/hook/usePacientes.jsx
+++
@@ -1,9 +0,0 @@
-//  este state   pasa por llamado todos los datos del context de pacientes
-import { useContext } from "react";
-import PacientesContext from "../context/PacienteProvaider";
-
-const usePacientes = () => {
-  return useContext(PacientesContext); // retorna el context de pacientes
-};
-
-export default usePacientes;
